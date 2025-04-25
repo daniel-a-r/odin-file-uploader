52a9013717e13f39620bd3755cffe4b9f6bc2816
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2025-04-25 18:30:59 -0400

    display dropdown on REGULAR folders

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,59 +1,69 @@
+const folderRoleElem = document.querySelector('[data-folder-role]');
+const folderRole = folderRoleElem.dataset.folderRole;
+
 const newFolderModal = document.querySelector('dialog.new-folder');
 const newFolderModalOpenButtons = document.querySelectorAll(
   'button.new-folder-open',
 );
-const newFolderClose = document.querySelector('button.new-folder-close');
-const folderActionsToggle = document.querySelector(
-  'button.folder-actions-toggle',
-);
-const folderActions = document.querySelector('div.folder-actions');
-const folderRenameModal = document.querySelector('dialog.folder-rename');
-const folderRenameOpen = document.querySelector('button.folder-rename-open');
-const folderRenameClose = document.querySelector('button.folder-rename-close');
-const folderDeleteOpen = document.querySelector('button.folder-delete-open');
-const folderDeleteModal = document.querySelector('dialog.folder-delete');
-const folderDeleteClose = document.querySelector('button.folder-delete-close');
-
 newFolderModalOpenButtons.forEach((button) => {
   button.addEventListener('click', () => {
     newFolderModal.showModal();
   });
 });
 
+const newFolderClose = document.querySelector('button.new-folder-close');
 newFolderClose.addEventListener('click', () => {
   newFolderModal.close();
 });
 
-newFolderModal.addEventListener('submit', () => {
-  console.log(window.location.pathname);
-});
+if (folderRole === 'REGULAR') {
+  // toggle folder actions dropdown
+  const folderActions = document.querySelector('div.folder-actions');
+  const folderActionsToggle = document.querySelector(
+    'button.folder-actions-toggle',
+  );
 
-folderActionsToggle.addEventListener('click', () => {
-  const display = window.getComputedStyle(folderActions).display;
-  folderActions.style.display = display === 'none' ? 'block' : 'none';
-});
-
-window.addEventListener('click', (e) => {
-  if (e.target !== folderActionsToggle) {
+  folderActionsToggle.addEventListener('click', () => {
     const display = window.getComputedStyle(folderActions).display;
-    if (display === 'block') {
-      folderActions.style.display = 'none';
+    folderActions.style.display = display === 'none' ? 'block' : 'none';
+  });
+
+  window.addEventListener('click', (e) => {
+    if (e.target !== folderActionsToggle) {
+      const display = window.getComputedStyle(folderActions).display;
+      if (display === 'block') {
+        folderActions.style.display = 'none';
+      }
     }
-  }
-});
+  });
 
-folderRenameOpen.addEventListener('click', () => {
-  folderRenameModal.showModal();
-});
+  // rename folder
+  const folderRenameModal = document.querySelector('dialog.folder-rename');
+  const folderRenameOpen = document.querySelector('button.folder-rename-open');
+  const folderRenameClose = document.querySelector(
+    'button.folder-rename-close',
+  );
 
-folderRenameClose.addEventListener('click', () => {
-  folderRenameModal.close();
-});
+  folderRenameOpen.addEventListener('click', () => {
+    folderRenameModal.showModal();
+  });
 
-folderDeleteOpen.addEventListener('click', () => {
-  folderDeleteModal.showModal();
-});
+  folderRenameClose.addEventListener('click', () => {
+    folderRenameModal.close();
+  });
 
-folderDeleteClose.addEventListener('click', () => {
-  folderDeleteModal.close();
-});
+  // delete folder
+  const folderDeleteOpen = document.querySelector('button.folder-delete-open');
+  const folderDeleteModal = document.querySelector('dialog.folder-delete');
+  const folderDeleteClose = document.querySelector(
+    'button.folder-delete-close',
+  );
+
+  folderDeleteOpen.addEventListener('click', () => {
+    folderDeleteModal.showModal();
+  });
+
+  folderDeleteClose.addEventListener('click', () => {
+    folderDeleteModal.close();
+  });
+}
