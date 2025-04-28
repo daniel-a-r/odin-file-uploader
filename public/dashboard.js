7903a9c87a9b7260026f9c709554e99c62575064
daniel-a-r <26808382+daniel-a-r@users.noreply.github.com>
2025-04-27 23:34:39 -0400

    refactor to put handlers in callback fn

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,3 +1,68 @@
+const resourceMethod = (event, resourceType, method) => {
+  const parentElement = event.target.parentElement;
+  const id = parentElement.dataset.id;
+  const resourceDeleteForm = document.querySelector(
+    `form.${resourceType}-${method}`,
+  );
+
+  if (id) {
+    resourceDeleteForm.action += `/${id}`;
+  }
+
+  if (method === 'delete') {
+    const resourceTitleSpan = document.querySelector(
+      `span.modal-info.${resourceType}-name`,
+    );
+    resourceTitleSpan.textContent = parentElement.dataset.name;
+  }
+
+  const modal = document.querySelector(`dialog.${resourceType}-${method}`);
+  modal.showModal();
+};
+
+const hanldeFolderRename = (event) => {
+  resourceMethod(event, 'folder', 'rename');
+};
+
+const handleFileRename = (event) => {
+  resourceMethod(event, 'file', 'rename');
+};
+
+const handleFolderDelete = (event) => {
+  resourceMethod(event, 'folder', 'delete');
+};
+
+const handleFileDelete = (event) => {
+  resourceMethod(event, 'file', 'delete');
+};
+
+const removeChildId = (resourceType, method) => {
+  // remove child resource id from end of URL if it exists
+  const form = document.querySelector(`form.${resourceType}-${method}`);
+  const pathnameArr = new URL(form.action).pathname.split('/');
+  if (pathnameArr.length === 5) {
+    form.action = pathnameArr.slice(0, -1).join('/');
+  }
+  const modal = document.querySelector(`dialog.${resourceType}-${method}`);
+  modal.close();
+};
+
+const handleFolderRenameClose = () => {
+  removeChildId('folder', 'rename');
+};
+
+const handleFolderDeleteClose = () => {
+  removeChildId('folder', 'delete');
+};
+
+const handleFileRenameClose = () => {
+  removeChildId('file', 'rename');
+};
+
+const handleFileDeleteClose = () => {
+  removeChildId('file', 'delete');
+};
+
 // folder create
 const folderCreateModal = document.querySelector('dialog.folder-create');
 const folderCreateModalOpenButtons = document.querySelectorAll(
@@ -36,103 +101,44 @@ const folderList = document.querySelectorAll('li.folder');
 
 if (folderRole === 'REGULAR' || folderList.length > 0) {
   // rename folder
-  const folderRenameModal = document.querySelector('dialog.folder-rename');
-
   const folderRenameOpenButtons = document.querySelectorAll(
     'button.folder-rename-open',
   );
   folderRenameOpenButtons.forEach((folderRenameOpen) => {
-    folderRenameOpen.addEventListener('click', (e) => {
-      // add child folder id to end of URL
-      const parentElement = e.target.parentElement;
-      const folderId = parentElement.dataset.folderId;
-      const folderRenameForm = document.querySelector('form.folder-rename');
-      if (folderId) {
-        folderRenameForm.action += `/${folderId}`;
-      }
-      folderRenameModal.showModal();
-    });
+    folderRenameOpen.addEventListener('click', hanldeFolderRename);
   });
 
   const folderRenameClose = document.querySelector(
     'button.folder-rename-close',
   );
-  folderRenameClose.addEventListener('click', () => {
-    // remove child folder id from end of URL if it exists
-    const folderRenameForm = document.querySelector('form.folder-rename');
-    const actionStrArr = folderRenameForm.action.split('/');
-    if (actionStrArr.length === 7) {
-      folderRenameForm.action = actionStrArr.slice(0, -1).join('/');
-    }
-    folderRenameModal.close();
-  });
+  folderRenameClose.addEventListener('click', handleFolderRenameClose);
 
   // delete folder
-  const folderDeleteModal = document.querySelector('dialog.folder-delete');
   const folderDeleteOpenButtons = document.querySelectorAll(
     'button.folder-delete-open',
   );
   folderDeleteOpenButtons.forEach((folderDeleteOpen) => {
-    folderDeleteOpen.addEventListener('click', (e) => {
-      // add child folder id to end of URL
-      const parentElement = e.target.parentElement;
-      const folderId = parentElement.dataset.folderId;
-      const folderDeleteForm = document.querySelector('form.folder-delete');
-      if (folderId) {
-        folderDeleteForm.action += `/${folderId}`;
-      }
-      const folderTitleSpan = document.querySelector(
-        'span.modal-info.folder-name',
-      );
-      folderTitleSpan.textContent = parentElement.dataset.folderName;
-      folderDeleteModal.showModal();
-    });
+    folderDeleteOpen.addEventListener('click', handleFolderDelete);
   });
 
   const folderDeleteClose = document.querySelector(
     'button.folder-delete-close',
   );
-  folderDeleteClose.addEventListener('click', () => {
-    // remove child folder id from end of URL if it exists
-    const folderDeleteForm = document.querySelector('form.folder-delete');
-    const actionStrArr = folderDeleteForm.action.split('/');
-    if (actionStrArr.length === 7) {
-      folderDeleteForm.action = actionStrArr.slice(0, -1).join('/');
-    }
-    folderDeleteModal.close();
-  });
+  folderDeleteClose.addEventListener('click', handleFolderDeleteClose);
 }
 
 const fileList = document.querySelectorAll('li.file');
 if (fileList.length > 0) {
   // rename file
-  const fileRenameModal = document.querySelector('dialog.file-rename');
   const fileRenameOpenButtons = document.querySelectorAll(
     'button.file-rename-open',
   );
   fileRenameOpenButtons.forEach((fileRenameOpen) => {
-    fileRenameOpen.addEventListener('click', (e) => {
-      // add child folder id to end of URL
-      const parentElement = e.target.parentElement;
-      const fileId = parentElement.dataset.id;
-      const fileRenameForm = document.querySelector('form.file-rename');
-      if (fileId) {
-        fileRenameForm.action += `/${fileId}`;
-      }
-      fileRenameModal.showModal();
-    });
+    fileRenameOpen.addEventListener('click', handleFileRename);
   });
 
   const fileRenameClose = document.querySelector('button.file-rename-close');
-  fileRenameClose.addEventListener('click', () => {
-    // remove child folder id from end of URL if it exists
-    const fileRenameForm = document.querySelector('form.file-rename');
-    const actionStrArr = fileRenameForm.action.split('/');
-    if (actionStrArr.length === 7) {
-      fileRenameForm.action = actionStrArr.slice(0, -1).join('/');
-    }
-    fileRenameModal.close();
-  });
+  fileRenameClose.addEventListener('click', handleFileRenameClose);
 
   // open file details modal
   const fileDetailsOpenButtons = document.querySelectorAll(
@@ -165,35 +171,15 @@ if (fileList.length > 0) {
   });
 
   // delete file
-  const fileDeleteModal = document.querySelector('dialog.file-delete');
   const fileDeleteOpenButtons = document.querySelectorAll(
     'button.file-delete-open',
   );
   fileDeleteOpenButtons.forEach((fileDeleteOpen) => {
-    fileDeleteOpen.addEventListener('click', (e) => {
-      // add child folder id to end of URL
-      const parentElement = e.target.parentElement;
-      const fileId = parentElement.dataset.id;
-      const fileDeleteForm = document.querySelector('form.file-delete');
-      if (fileId) {
-        fileDeleteForm.action += `/${fileId}`;
-      }
-      const fileTitleSpan = document.querySelector('span.modal-info.file-name');
-      fileTitleSpan.textContent = parentElement.dataset.name;
-      fileDeleteModal.showModal();
-    });
+    fileDeleteOpen.addEventListener('click', handleFileDelete);
   });
 
   const fileDeleteClose = document.querySelector('button.file-delete-close');
-  fileDeleteClose.addEventListener('click', () => {
-    // remove child file id from end of URL if it exists
-    const fileDeleteForm = document.querySelector('form.file-delete');
-    const pathnameArr = new URL(fileDeleteForm.action).pathname.split('/');
-    if (pathnameArr.length === 5) {
-      fileDeleteForm.action = pathnameArr.slice(0, -1).join('/');
-    }
-    fileDeleteModal.close();
-  });
+  fileDeleteClose.addEventListener('click', () => handleFileDeleteClose);
 }
 
 const dropdownToggleButtons = document.querySelectorAll(
